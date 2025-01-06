033f9386418b35bcf035948d8efdeb3db766d5d8
unknown <youssefabdulmoneim@gmail.com>
2025-01-06 21:05:08 +0200

    Django Project

--- JS/user.js
+++
@@ -1,8 +0,0 @@
-// class User {
-//   constructor(name, email, password, isCompanyAdmin) {
-//     this.name = name;
-//     this.email = email;
-//     this.password = password;
-//     this.isCompanyAdmin = isCompanyAdmin;
-//   }
-// }
